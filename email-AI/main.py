9565340dc76dccae4eebc838d321f3e4ad29e15b
sofia <sofiapchini@gmail.com>
2026-01-13 18:28:26 -0300

    erros de sintaxe na main

--- email-AI/main.py
+++ email-AI/main.py
@@ -11,13 +11,14 @@ class EmailInput(BaseModel):
 
 @app.post("/analyze")
 def analisar_email(data: EmailInput):
-    texto =- data.text.lower()
+    texto = data.text.lower()
 
     if any(p in texto for p in ["suporte", "ajuda", "erro", "problema"]):
         categoria = "Produtivo"
         resp = ("Olá, recebemos sua solicitação e estamos trabalhando para resolvê-la.")
-    categoria = "Improdutivo"
-    resp = "Olá, recebemos a sua mensagem. Estamos à disposição caso precise de algo"
+    else:
+        categoria = "Improdutivo"
+        resp = "Olá, recebemos a sua mensagem. Estamos à disposição caso precise de algo"
 
     return {
         "categoria": categoria,
