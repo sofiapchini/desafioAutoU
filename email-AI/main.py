fc2d1e73e80746c17a55d28b148a5001410cbaf9
sofia <sofiapchini@gmail.com>
2026-01-13 22:08:35 -0300

    comentários e remoção de imports redundantes

--- email-AI/main.py
+++ email-AI/main.py
@@ -3,19 +3,19 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from typing import Optional
 from pypdf import PdfReader
-import os 
-import requests
 from openai import OpenAI
 
+#carrega variaveis de ambiente local para obfuscar a api key
 from dotenv import load_dotenv
 load_dotenv()
-
 app = FastAPI()
 
+#configuração dos templates HTML
 templates = Jinja2Templates(directory="templates")
 
 client = OpenAI()
 
+# prompt para classificação e geração de resposta (default: Improdutivo)
 def classificar_email_com_ia(texto: str) -> dict:
     prompt = f"""
 Você é um assistente responsável por TRIAGEM de emails de suporte.
@@ -46,10 +46,12 @@ Resposta: <texto>
     )
 
     texto_saida = response.choices[0].message.content
-
+    
+    #esses valores são um método de fallback, só são utilizados caso o prompt falhe
     categoria = "Improdutivo"
     resposta = "Agradecemos sua mensagem."
 
+    #check pra categoria, foi a primeira ideia que eu tive (e que funcionou), mas acredito que possa ser melhorado
     for line in texto_saida.splitlines():
         line_lower = line.lower()
         if line_lower.startswith("categoria"):
@@ -67,7 +69,7 @@ Resposta: <texto>
     }
 
 
-
+#endpoint http get
 @app.get("/", response_class=HTMLResponse)
 def home(request: Request):
     return templates.TemplateResponse(
@@ -75,7 +77,7 @@ def home(request: Request):
         {"request": request}
     )
 
-
+#endpoint http post
 @app.post("/analyze", response_class=HTMLResponse)
 async def analyze_email(
     request: Request,
@@ -84,6 +86,7 @@ async def analyze_email(
 ):
     email_text = ""
 
+    #arquivos tem preferencia, caso o arquivo exista, é checado sua extensão e é salvo seu filename
     if file and file.filename:
         filename = file.filename.lower()
 
@@ -97,21 +100,23 @@ async def analyze_email(
                 page_text = page.extract_text()
                 if page_text:
                     email_text += page_text
+        #caso o arquivo não tenha a extensão apropriada, um erro é retornado
         else:
             return templates.TemplateResponse(
                 "index.html",
                 {"request": request, "error": "Formato não suportado"}
             )
-
+    #caso file ou filename seja None (arquivo não foi enviado), o programa checa se algum texto foi enviado.
     elif text and text.strip():
         email_text = text.strip()
-
+        
+    #caso até o texto não estiver sido enviado, uma mensagem de erro e instrução ao usuário é enviada
     else:
         return templates.TemplateResponse(
             "index.html",
             {"request": request, "error": "Envie texto ou arquivo"}
         )
-
+    #se o texto não foi extraído com sucesso, então imprima mensagem de erro ao usuário
     if not email_text.strip():
         return templates.TemplateResponse(
             "index.html",
@@ -120,13 +125,14 @@ async def analyze_email(
 
     texto = " ".join(email_text.lower().split())
 
+    # esse bloco try chama a funcao de IA, caso a função funcione, salva o seu diciontario resultante na variavel resultado
     try:
         resultado = classificar_email_com_ia(texto)
         categoria = resultado["categoria"]
         resposta = resultado["resposta"]
 
+    #caso a função não execute é necessário parsar manualmente o texto e checar pelas keywords de emails produtivos
     except Exception as e:
-        print("ERRO:", e)
 
         palavras_produtivas = [
             "status",
@@ -137,7 +143,7 @@ async def analyze_email(
             "acesso",
             "chamado",
         ]
-
+        #parsing automatico de cada palavra presente no texto, caso alguma palavra for encontrada o email é classificado como "produtivo"
         if any(p in texto.lower() for p in palavras_produtivas):
             categoria = "Produtivo"
             resposta = "Recebemos sua solicitação e em breve retornaremos."
@@ -145,6 +151,7 @@ async def analyze_email(
             categoria = "Improdutivo"
             resposta = "Agradecemos sua mensagem."
 
+    #retorna pro frontend o resultado
     return templates.TemplateResponse(
         "index.html",
         {
